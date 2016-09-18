db4ecf4bbcaa632132fbce6a1c0d5eae41100c02
venkadesan-somu <venkadesan.somu@a-cti.com>
2016-09-18 15:25:10 +0530

    Add files via upload

---
+++ Version 6/js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+// 	$('a.menu-bar').click(function(){
+// 		// console.log("Li clicked");
+		
+// 		if( $("nav").hasClass("responsive")){
+// 			$("nav").removeClass("responsive");
+// 		}
+// 		else{
+// 			$("nav").addClass('responsive');
+// 		}
+// 	});
+// });
